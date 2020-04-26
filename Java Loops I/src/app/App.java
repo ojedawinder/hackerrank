a29dbb45cdddd4a5d4b807c493189952c71bd468
Winder <Ojeda>
2020-04-26 14:18:55 -0300

    Ready!

---
+++ Java Loops I/src/app/App.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.util.Scanner;
+
+public class App {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for(Integer i = 1; i <= 10; i++)
+            System.out.printf("%d x %d = %d%n", N, i, (N*i));
+        scanner.close();
+    }
+}

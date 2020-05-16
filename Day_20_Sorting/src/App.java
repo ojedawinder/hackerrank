5069a5ef09147e7977404a9be1ba4a5944b88048
Winder <Ojeda>
2020-05-16 11:03:47 -0300

    Day 20

---
+++ Day_20_Sorting/src/App.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        // Track number of elements swapped during a single array traversal
+        int numberOfSwaps = 0;
+            
+        for (int i = 0; i < n; i++) {
+            
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1]=temp;
+                    //swap(a[j], a[j + 1]);
+                    numberOfSwaps++;
+                }
+            }
+            
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+
+        System.out.printf("Array is sorted in %d swaps.%n", numberOfSwaps);
+        System.out.printf("First Element: %d%n", a[0]);
+        System.out.printf("Last Element: %d%n", a[n-1]);
+
+    }
+}
